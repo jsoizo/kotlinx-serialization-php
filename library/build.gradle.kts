6b5423800b9d433e77d2b190827464f24e38bb84
Jun Sekine <jun@sakamo.to>
2024-12-27 22:44:13 +0900

    [skip-ci] mavenPublish task

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
@@ -8,8 +9,11 @@ plugins {
     alias(libs.plugins.powerAssert)
     alias(libs.plugins.kover)
     alias(libs.plugins.ktlint)
-//    id("module.publication")
+    alias(libs.plugins.mavenPublish)
 }
+
+group = "com.jsoizo"
+version = "0.1.0"
 
 kotlin {
     jvm()
@@ -39,16 +43,47 @@ kotlin {
     }
 }
 
+android {
+    namespace = "com.jsoizo.serialization.php"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+
 @OptIn(ExperimentalKotlinGradlePluginApi::class)
 powerAssert {
     functions = listOf("kotlin.assert", "kotlin.test.assertTrue", "kotlin.test.assertEquals", "kotlin.test.assertNull")
     includedSourceSets = listOf("commonMain")
 }
 
-android {
-    namespace = "com.jsoizo.serialization.php"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates(group.toString(), "library", version.toString())
+
+    pom {
+        name = "kotlinx-serialization-php"
+        description = "PHP support for kotlinx.serialization"
+        inceptionYear = "2024"
+        url = "https://github.com/jsoizo/kotlinx-serialization-php"
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://github.com/jsoizo/kotlinx-serialization-php/blob/master/LICENSE")
+            }
+        }
+        scm {
+            url.set("https://github.com/jsoizo/kotlinx-serialization-php")
+            connection.set("scm:git:git://github.com/jsoizo/kotlinx-serialization-php.git")
+            developerConnection.set("https://github.com/jsoizo/kotlinx-serialization-php")
+        }
+        developers {
+            developer {
+                name.set("jsoizo")
+            }
+        }
     }
 }
